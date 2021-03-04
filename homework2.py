07a146b35ca343c7e76e1f5c572db216e6609415
JUDY MAKAKA <79365316+makakajudy@users.noreply.github.com>
2021-03-04 19:50:21 +0000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,28 @@
+# assignment 2:function derived from the attributes
+
+# this function returns true if the entered views matches the set views 9.3.
+def views_in_millions(views):
+    return bool(views == 9.3)  # same as return x==9.3
+
+
+# returns the variable content passed to parameter y
+def language(y): return y
+
+
+# returns the variable content passed to parameter m
+
+def message(m): return m
+
+
+# # returns the variable content passed to parameter country
+def country_origin(country): return country
+
+
+content = "love is sweet like sugar/sukari"
+lang = "Swahili"
+origin = "Tanzania"
+
+print(views_in_millions(9.8))
+print(language(lang))
+print(message(content))
+print(country_origin(origin))
